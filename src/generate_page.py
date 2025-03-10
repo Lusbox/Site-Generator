c311572b62fbcf534dbfa6403a6200ebdf4b9332
Lusbox <samlusby4@gmail.com>
2025-03-10 19:16:29 +0000

    ok now

--- src/generate_page.py
+++ src/generate_page.py
@@ -26,8 +26,10 @@ def generate_page(from_path, template_path, dest_path, basepath):
 
 	title = extract_title(md_content)
 
-	template = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
-	template = template.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
+	template = template_content.replace("{{ Title }}", title)
+	template = template.replace("{{ Content }}", html_string)
+	template = template.replace('href="/', f'href="{basepath}')
+	template = template.replace('src="/', f'src="{basepath}')
 
 	dir_path = os.path.dirname(dest_path)
 	os.makedirs(dir_path, exist_ok=True)
--- src/main.py
+++ src/main.py
@@ -5,13 +5,16 @@ from copy_dir import *
 from generate_pages import *
 
 def main():
+	basepath = sys.argv[1] if len(sys.argv) > 1 else '/'
+	print(f"Using basepath: {basepath}")
+
 	test_object = TextNode("anchor_text", TextType.LINK, "https://www.boot.dev")
 	print(repr(test_object))
 
-basepath = sys.argv[1] if len(sys.argv) > 1 else '/'
-print(f"Using basepath: {basepath}")
 
-main()
-copy_to_dir("static", "docs")
+	copy_to_dir("static", "docs")
 
-generate_pages_recursive("content", "template.html", "docs", basepath)
+	generate_pages_recursive("content", "template.html", "docs", basepath)
+
+if __name__ == "__main__":
+	main()
